ac82d49b366dd8dae78d3c80c55a34d541ffd743
theakanksha02 <theakankshaa@gmail.com>
2017-11-06 07:47:01 +0530

    activity added

--- app/src/main/java/com/bignerdranch/android/learnandroid/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/learnandroid/MainActivity.java
@@ -1,7 +1,10 @@
 package com.bignerdranch.android.learnandroid;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -9,9 +12,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mCheatButton;
+    private static final String KEY_INDEX = "index";
+    private static final int REQUEST_CODE_CHEAT = 0;
+
     private TextView mQuestionTextView;
     private Question[] mQuestionBank = new Question[]
             {
@@ -20,29 +28,36 @@ public class MainActivity extends AppCompatActivity {
                     new Question(R.string.q3,true)
             };
     private int mCurrentIndex=0;
+    private boolean mIsCheater;
+
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
     }
 
-    private void checkAnswer(boolean userPressedTrue)
-    {
+    private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-        int messageResId= 0;
-        if(userPressedTrue== answerIsTrue)
-        {
-            messageResId=R.string.correct_toast;
+        int messageResId = 0;
+        if (mIsCheater) {
+            messageResId = R.string.judgement_toast;
         }
-        else
-        {
-            messageResId= R.string.incorrect_toast;
+        else {
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
         }
-        Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         updateQuestion();
         mTrueButton = (Button) findViewById(R.id.true_button);
@@ -64,8 +79,35 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mCurrentIndex= (mCurrentIndex+1)% mQuestionBank.length;
+                mIsCheater = false;
                 updateQuestion();
             }
         });
+        mCheatButton= findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean answerIsTrue= mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent i= CheatActivity.newIntent(MainActivity.this,answerIsTrue);
+                startActivityForResult(i, REQUEST_CODE_CHEAT);
+            }
+        });
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        }
+        updateQuestion();
+        }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            if (data == null) {
+                return;
+            }
+            mIsCheater = CheatActivity.wasAnswerShown(data);
+        }
     }
-}
+ }
